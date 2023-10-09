9fb479c518ca6a9d37507125e92faa96f1880ef5
BinduMuniyappa <muniyappabindu7@gmail.com>
2023-10-09 17:48:29 +0530

    created react admin panel

---
+++ Admin_panel/src/App.jsx
@@ -0,0 +1,23 @@
+import { useState } from 'react'
+import './App.css'
+import Home from './Home'
+import Header from './Header'
+import Sidebar from './Sidebar'
+
+function App() {
+  const [OpenSidebarToggle,setOpenSidebarToggle]=useState(false)
+
+  const OpenSidebar=()=>{
+    setOpenSidebarToggle(!OpenSidebarToggle)
+  }
+
+  return (
+   <div className='grid-container'>
+    <Header OpenSidebar={OpenSidebar}/>
+    <Sidebar OpenSidebarToggle={OpenSidebarToggle} OpenSidebar={OpenSidebar}/>
+    <Home/>
+   </div>
+  )
+}
+
+export default App
